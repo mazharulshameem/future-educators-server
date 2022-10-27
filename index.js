d5d988591ebe67e6b796500980decfa4698cd1c5
Mazharul Shameem <mazharulis.cs.07@gmail.com>
2022-10-27 13:18:23 +0600

    each course data added

--- index.js
+++ index.js
@@ -8,6 +8,10 @@ app.use(cors());
 const categories = require("./Data/categories.json");
 const courses = require("./Data/courses.json");
 
+app.get("/", (req, res) => {
+  res.send("api ggggggg running");
+});
+
 app.get("/course-categories", (req, res) => {
   res.send(categories);
 });
@@ -18,8 +22,11 @@ app.get("/courses/:id", (req, res) => {
   res.send(category_course);
 });
 
-app.get("/courses/:id", (req, res) => {
+app.get("/course/:id", (req, res) => {
   const id = req.params.id;
   const selectedCourse = courses.find((n) => n.id === id);
   res.send(selectedCourse);
+});
+app.listen(port, () => {
+  console.log(" running ", port);
 });
